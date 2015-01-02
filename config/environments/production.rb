32d570b4756564b37f4bc41a636abb9351a1269f
Landon Mossburg <wonderbeast@gmail.com>
2015-01-02 12:20:58 -0800

    update mail configs for digitalocean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
     
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'trym.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'trym.io' }
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets[:smtp_host],
