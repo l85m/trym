5dfc8dfac490f6631149d2ad656637e9d7ad7938
Landon Mossburg <wonderbeast@gmail.com>
2015-02-09 09:34:42 -0800

    patch slide transitions on bootstrap carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,13 @@
 //= require jquery-ui/effect
 //= require select2
 //= require jquery.form-validator
+
+// jQuery.support.transition
+// to verify that CSS3 transition is supported (or any of its browser-specific implementations)
+$.support.transition = (function(){
+    var thisBody = document.body || document.documentElement,
+    thisStyle = thisBody.style,
+    support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
+    
+    return support;
+})();
