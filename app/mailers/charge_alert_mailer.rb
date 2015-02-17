5fadc9282e159135facb22141b63a45d288a8c3a
Landon Mossburg <wonderbeast@gmail.com>
2015-02-16 16:27:59 -0800

    charge alert mailer

--- app/mailers/charge_alert_mailer.rb
+++ app/mailers/charge_alert_mailer.rb
@@ -1,7 +1,8 @@
 class ChargeAlertMailer < ApplicationMailer
   
-  def three_days_before_charge(user)
+  def three_days_before_charge(user, charges)
     @user = user
-    mail to: @user.email, subjet: "3 days until your next charge", skip_premailer: true
+    @charges = charges
+    mail to: @user.email, subject: "Charge Alert: Three days until next charges for $#{(@charges.collect(&:amount).inject(:+) / 100.0).to_i}"
   end
 end
