cff2bb053300ca7a79e14445cdaec7ce5c180880
Landon Mossburg <wonderbeast@gmail.com>
2015-03-11 15:35:34 -0700

    fix bug on migration

--- db/migrate/20150225004100_change_ownership_of_charge_wizard.rb
+++ db/migrate/20150225004100_change_ownership_of_charge_wizard.rb
@@ -1,6 +1,5 @@
 class ChangeOwnershipOfChargeWizard < ActiveRecord::Migration
   def change
   	remove_reference :charge_wizards, :linked_account, index: true
-  	add_reference :charge_wizards, :user, index: true
   end
 end
