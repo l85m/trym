4a8af0587f2d0aa0861afda2da2f058e384b7199
demosophy <wonderbeast@gmail.com>
2015-05-01 22:06:55 -0700

    more mail stuff

--- app/mailers/charge_alert_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ChargeAlertMailer < ApplicationMailer
-  
-  def three_days_before_charge(user, charges)
-    @user = user
-    @charges = charges
-    mail to: @user.email, subject: "Charge Alert: Three days until next charges for $#{(@charges.collect(&:amount).inject(:+) / 100.0).to_i}"
-  end
-end
