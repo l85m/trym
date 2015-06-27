c4dd07ce50fe8782b190ed6fb4611382ea4182f3
Landon Mossburg <landon.mossburg@gmail.com>
2015-06-27 11:20:26 -0700

    make trym category rename migration less brittle

--- db/migrate/20150621124813_change_trym_category_names.rb
+++ db/migrate/20150621124813_change_trym_category_names.rb
@@ -1,14 +1,24 @@
 class ChangeTrymCategoryNames < ActiveRecord::Migration
   def up
-		TrymCategory.find_by_name("TV, Phone, & Internet Providers").update(name: "TV, Phone, & Internet")
+		t = TrymCategory.find_by_name("TV, Phone, & Internet Providers")
+    if t.present? 
+      t.update(name: "TV, Phone, & Internet")
+    end
+
 		t = TrymCategory.find_by_name("Storage, Utilities, Mortgage & Rent")
-		t.charges.update_all( trym_category_id: TrymCategory.find_by_name("Home Services & Security") )
-		t.delete
-		TrymCategory.create( name: "Other", recurring: true, description: "All other recurring expenses" )
+		if t.present?
+      t.charges.update_all( trym_category_id: TrymCategory.find_by_name("Home Services & Security") )
+  		t.delete
+    end
+		
+    TrymCategory.create( name: "Other", recurring: true, description: "All other recurring expenses" ) unless TrymCategory.find_by_name("Other").present?
   end
 
   def down
-  	TrymCategory.find_by_name("TV, Phone, & Internet").update("TV, Phone, & Internet Providers")
+  	t = TrymCategory.find_by_name("TV, Phone, & Internet")
+    if t.present?
+      t.update("TV, Phone, & Internet Providers")
+    end
   	TrymCategory.create( name: "Storage, Utilities, Mortgage & Rent", recurring: true, description: "Public Storage, Power, Water, Gas, Rent, Mortgage Payments" )
   end
 
