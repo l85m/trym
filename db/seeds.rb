12bcc0b4ad8936f4978eb310bc0170724d693ab1
demosophy <wonderbeast@gmail.com>
2015-06-27 11:24:54 -0700

    seed trym categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+TrymCategory.create(name: 'Gym & Club Memberships', recurring: true, description: 'Fitness Memberships, Golf & Country Club Dues, Social Club Dues, ect.')
+TrymCategory.create(name: 'Insurance', recurring: true, description: 'State Farm, Geico, Allstate, Farmers, ect.')
+TrymCategory.create(name: 'Home Services & Security', recurring: true, description: 'Landscaping, Security Monitoring Fees, Cleaning Services')
+TrymCategory.create(name: 'Newspaper & Magazines', recurring: true, description: "New York Times, Wall Street Journal, Better Homes & Gardens, Reader's Digest, ect.")
+TrymCategory.create(name: 'Transportation Services', recurring: true, description: "ZipCar, Public Transport Pass, Car Share, Plug Share")
+TrymCategory.create(name: 'Financial Services', recurring: true, description: "Checking Account Maintenance Fees, Credit Card Annual Fees, Credit Reports, Investment Management Fees, ect.")
+TrymCategory.create(name: 'Shopping & Home Delivery', recurring: true, description: "Amazon Prime, Costco, Sam's Club, Stitchfix, Bombfell, ect.")
+TrymCategory.create(name: 'Online Services', recurring: true, description: 'Subscription Websites, AOL, Match.com, Amazon Web Services, Dropbox, Audible.com, TheLadders.com, ect.')
+TrymCategory.create(name: 'Other', recurring: true, description: 'All other recurring expenses')
+TrymCategory.create(name: 'TV, Phone, & Internet', recurring: true, description: 'Comcast, Time Warner, Charter, Verizon, AT&T, T-Mobile, Sprint, Hulu, Netflix, HBO, ect.')
