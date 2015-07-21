0cb5e9aa0169f8709da576fe228d25ab7ec56532
demosophy <wonderbeast@gmail.com>
2015-07-20 18:05:19 -0700

    restrict access to admins

--- app/controllers/merchant_aliases_controller.rb
+++ app/controllers/merchant_aliases_controller.rb
@@ -1,6 +1,18 @@
 class MerchantAliasesController < InheritedResources::Base
+  before_action :authenticate_user!
+  before_action :authenticate_admin!
+
+  def index
+  	@merchant_aliases = MerchantAlias.all
+  end
 
   private
+
+  	def authenticate_admin!
+  		unless current_user.admin
+  			access_denied(nil)
+  		end
+  	end
 
     def merchant_alias_params
       params.require(:merchant_alias).permit(:alias, :merchant_id, :financial_institution_id)
