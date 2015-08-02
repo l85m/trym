19b53a6da841deada2df1bd0e8a08c806ac295bc
demosophy <wonderbeast@gmail.com>
2015-08-02 14:34:07 -0700

    show warning when ip doesn't appear to be from US

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || charges_path
   end
@@ -14,5 +14,16 @@ class ApplicationController < ActionController::Base
 	def not_found
 	  raise ActionController::RoutingError.new('Not Found')
 	end
+
+  private
+
+  def show_location_restriction
+    unless current_user
+      loc = GeoIP.new(Rails.root.join("GeoLiteCity.dat")).city(request.remote_ip)
+      if loc && loc.country_code2 != "US"
+        flash[:notice] = "You appear to be visiting from a location outside of the US.  Please note that currently trym can only support linking and managing accounts that are based in the US."
+      end
+    end
+  end
 
 end
